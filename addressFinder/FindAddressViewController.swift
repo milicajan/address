97a0db0c153777739d3ad1d47e61a1d97c8d62fb
Milica Jankovic <m.jankovic@Administrators-Mac-mini.local>
2017-11-03 16:31:19 +0100

    Move text field above keyboard

--- addressFinder/FindAddressViewController.swift
+++ addressFinder/FindAddressViewController.swift
@@ -22,7 +22,7 @@ class FindAddressViewController: UIViewController, UITextFieldDelegate {
     @IBOutlet weak var cityLabel: UILabel!
     @IBOutlet weak var stateLabel: UILabel!
     @IBOutlet weak var postalLabel: UILabel!
-    @IBOutlet weak var postalErrorLabel: UILabel!
+ 
     
     @IBOutlet weak var searchButton: UIButton!
     
@@ -34,7 +34,7 @@ class FindAddressViewController: UIViewController, UITextFieldDelegate {
     @IBOutlet var contentView: UIView!
     @IBOutlet weak var scrollView: UIScrollView!
     
-    // View lifecycle
+    // MARK: View lifecycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -43,42 +43,73 @@ class FindAddressViewController: UIViewController, UITextFieldDelegate {
         cityTextField.delegate = self
         stateTextField.delegate = self
         postalTextField.delegate = self
+        postalTextField.keyboardType = UIKeyboardType.numberPad
         
         
-        self.view.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(FindAddressViewController.dismissKeyboard)))
+        self.view.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(didTapView(gesture:))))
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(true)
+        //addObservers()
     }
     
     override func viewWillDisappear(_ animated: Bool) {
-        dismissKeyboard()
+        super.viewWillDisappear(true)
+        
+        addressTextField.resignFirstResponder()
+        cityTextField.resignFirstResponder()
+        stateTextField.resignFirstResponder()
+        postalTextField.resignFirstResponder()
+        
+       // removeObservers()
     }
     
     
+    // MARK: Actions
+    
     @IBAction func searchLocationButton(_ sender: UIButton) {
         
-        resetTextField()
+        resetTextField(addressUnderlineView, addressLabel)
+        resetTextField(cityUnderlineView, cityLabel)
+        resetTextField(cityUnderlineView, cityLabel)
+        resetTextField(postalUnderlineView, postalLabel)
         
         if let address = addressTextField.text, !address.isEmpty {
             if let city = cityTextField.text, !city.isEmpty {
                 if let state = stateTextField.text, !state.isEmpty {
-                    if let postal = postalTextField.text, !postal.isEmpty {
-                        if (postal.characters.count > 1 && postal.characters.count < 7) {
-                            
+                    if let postal = postalTextField.text, !postal.isEmpty, (postal.characters.count > 1 && postal.characters.count < 7)
+                        {
                             
                             let fullAddress = ("\(address) \(city) \(state) \(postal)")
                             print("Address is \(fullAddress)")
-                        }
+                        
                     } else {
-                        setTextFieldWhenError()
+                        setTextFieldWhenError(addressUnderlineView, addressLabel)
+                        setTextFieldWhenError(cityUnderlineView, cityLabel)
+                        setTextFieldWhenError(cityUnderlineView, cityLabel)
+                        setTextFieldWhenError(postalUnderlineView, postalLabel)
                     }
                 } else {
-                    setTextFieldWhenError()
+                    setTextFieldWhenError(addressUnderlineView, addressLabel)
+                    setTextFieldWhenError(cityUnderlineView, cityLabel)
+                    setTextFieldWhenError(cityUnderlineView, cityLabel)
+                    setTextFieldWhenError(postalUnderlineView, postalLabel)
+                    
                 }
             } else {
-                setTextFieldWhenError()
+                setTextFieldWhenError(addressUnderlineView, addressLabel)
+                setTextFieldWhenError(cityUnderlineView, cityLabel)
+                setTextFieldWhenError(cityUnderlineView, cityLabel)
+                setTextFieldWhenError(postalUnderlineView, postalLabel)
+                
             }
         } else {
             
-            setTextFieldWhenError()
+            setTextFieldWhenError (addressUnderlineView, addressLabel)
+            setTextFieldWhenError(cityUnderlineView, cityLabel)
+            setTextFieldWhenError(cityUnderlineView, cityLabel)
+            setTextFieldWhenError(postalUnderlineView, postalLabel)
         }
     }
     
@@ -90,58 +121,87 @@ class FindAddressViewController: UIViewController, UITextFieldDelegate {
     
     // MARK: UITextFieldDelegate methode
     func textFieldDidBeginEditing(_ textField: UITextField) {
-        if textField.isFirstResponder == true {
-            textField.placeholder = ""
-        }
+       scrollView.setContentOffset(CGPoint(x: 0,y:  100), animated: true)
     }
     
-    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        scrollView.setContentOffset(CGPoint(x: 0,y: 0), animated: true)
+    }
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-        self.addressTextField.resignFirstResponder()
-        self.cityTextField.resignFirstResponder()
-        self.stateTextField.resignFirstResponder()
-        self.postalTextField.resignFirstResponder()
+        textField.resignFirstResponder()
+        scrollView.setContentOffset(CGPoint(x: 0,y:  0), animated: true)
         return true
         
     }
+    /*func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        
+            let allowedCharacters = CharacterSet.decimalDigits
+            let characterSet = CharacterSet(charactersIn: string)
+            return allowedCharacters.isSuperset(of: characterSet)
+        }*/
     
-    func dismissKeyboard(){
-        self.addressTextField.resignFirstResponder()
-        self.cityTextField.resignFirstResponder()
-        self.stateTextField.resignFirstResponder()
-        self.postalTextField.resignFirstResponder()
+    
+    func didTapView(gesture: UITapGestureRecognizer) {
+        view.endEditing(true)
     }
     
     
     // UIView and UILabel color setting methodes
     
-    func resetTextField() {
+    func resetTextField(_ view: UIView, _ label: UILabel) {
         
-        addressUnderlineView.backgroundColor = UIColor(red: 0/255.0, green: 112.0/255.0, blue: 140.0/255.0, alpha: 1.0)
-        cityUnderlineView.backgroundColor = UIColor(red: 0/255.0, green: 112.0/255.0, blue: 140.0/255.0, alpha: 1.0)
-        stateUnderlineView.backgroundColor = UIColor(red: 0/255.0, green: 112.0/255.0, blue: 140.0/255.0, alpha: 1.0)
-        postalUnderlineView.backgroundColor = UIColor(red: 0/255.0, green: 112.0/255.0, blue: 140.0/255.0, alpha: 1.0)
-        
-        addressLabel.isHidden = true
-        cityLabel.isHidden = true
-        stateLabel.isHidden = true
-        postalLabel.isHidden = true
-        postalErrorLabel.isHidden = true
+        view.backgroundColor = UIColor(red: 0/255.0, green: 112.0/255.0, blue: 140.0/255.0, alpha: 1.0)
+        label.isHidden = true
         
     }
     
-    func setTextFieldWhenError() {
-        addressUnderlineView.backgroundColor = UIColor(red: 225.0/255.0, green: 64.0/255.0, blue: 76.0/255.0, alpha: 1.0)
-        cityUnderlineView.backgroundColor = UIColor(red: 225.0/255.0, green: 64.0/255.0, blue: 76.0/255.0, alpha: 1.0)
-        stateUnderlineView.backgroundColor = UIColor(red: 225.0/255.0, green: 64.0/255.0, blue: 76.0/255.0, alpha: 1.0)
-        postalUnderlineView.backgroundColor = UIColor(red: 225.0/255.0, green: 64.0/255.0, blue: 76.0/255.0, alpha: 1.0)
+    func setTextFieldWhenError(_ view: UIView, _ label: UILabel) {
+        view.backgroundColor = UIColor(red: 225.0/255.0, green: 64.0/255.0, blue: 76.0/255.0, alpha: 1.0)
+        label.isHidden = false
         
-        addressLabel.isHidden = false
-        cityLabel.isHidden = false
-        stateLabel.isHidden = false
-        postalLabel.isHidden = false
     }
     
+    func setTextFieldWhenNumberError(_ view: UIView, _ label: UILabel) {
+        
+        view.backgroundColor = UIColor(red: 225.0/255.0, green: 64.0/255.0, blue: 76.0/255.0, alpha: 1.0)
+        label.isHidden = false
+    }
+    
+    // MARK: NotificationCenter
+    
+   /* func addObservers() {
+        NotificationCenter.default.addObserver(forName: .UIKeyboardWillShow, object: nil, queue: nil) {
+            notification in
+            self.keyboardWillShow(notification: notification)
+        }
+        
+        NotificationCenter.default.addObserver(forName: .UIKeyboardWillHide, object: nil, queue: nil) {
+            notification in
+            self.keyboardWillHide(notification: notification)
+        }
+    }
+    
+    func removeObservers() {
+        NotificationCenter.default.removeObserver(self)
+    }
+    // MARK: Keyboard handaling notifications
+    
+    func keyboardWillShow(notification: Notification) {
+        guard let userInfo = notification.userInfo, let frame = (userInfo[UIKeyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue else {
+            return
+        }
+        
+        
+        scrollView.setContentOffset(CGPoint(x: 0,y:  250), animated: true)
+    }
+    
+    
+    func keyboardWillHide(notification: Notification) {
+        scrollView.contentInset = UIEdgeInsets.zero
+    }*/
+ 
+    
+}
     // MARK: Web service
     //func findAddress(fullAddress: String!, withCompletionHandlet completionHandler: ((_ status: String, _ success: Bool) -> Void)) {
     //   let baseURLGeocode = "https://sampleserver1.arcgisonline.com/ArcGIS/rest/services/Locators/ESRI_Geocode_USA/GeocodeServer/findAddressCandidates/json?"
@@ -153,7 +213,15 @@ class FindAddressViewController: UIViewController, UITextFieldDelegate {
     //     var geocodeURLString = baseURLGeocode + "Address=" + findAddress
     
     // }
-}
+
 
 // https://sampleserver1.arcgisonline.com/ArcGIS/rest/services/Locators/ESRI_Geocode_USA/GeocodeServer/findAddressCandidates?Address=1156+High+Street&City=Santa+Cruz&State=CA&Zip=95064&outFields=&outSR=&f=pjson
+/*extension UITextField {
+    func isNumeric(textField: UITextField) -> Bool {
+        if (textField.text!.characters.count < 7 && textField.text!.characters.count > 1) {
+            let numbers: Set<Character> = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
+            return Set(self.text!.characters).isSubset(of: numbers)
+        }
+        return false
+    }*/
 
